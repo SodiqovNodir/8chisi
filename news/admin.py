18fa23495eee0aaf612696483f0e839f97667230
Nodirbek <sodiqovnodirbek14@gmail.com>
2024-12-23 15:24:27 +0500

    +

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 
 from .models import Course, Lesson
 
-admin.site.register(Course)
-admin.site.register(Lesson)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_photo','created')
+    list_display_links = ('name',)
+    search_fields = ('name', 'content', 'created')
+    list_filter = ('name',)
+
+    def get_photo(self, course):
+        if course.photo:
+            return mark_safe(f'<img src="{course.photo.url}" width=150px')
+        return '-'
+
+admin.site.register(Course, CourseAdmin)
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created', 'course')
+    list_editable = ('course',)
+    list_display_links = ('name',)
+    list_filter = ('course',)
+    search_fields = ('name', 'content', 'created')
+
+admin.site.register(Lesson, LessonAdmin)
